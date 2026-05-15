338bce355b16af9e0c32c5deb6d64dbb415c60e9
Balrogv1 <2424610757@qq.com>
2026-05-15 20:14:58 +0800

    only track chapt1 workspace

--- chapt2_ws/src/demo_python_pkg/demo_python_pkg/py_node2.py
+++
@@ -1,9 +0,0 @@
-import rclpy
-from rclpy.node import Node
-
-def main():
-    rclpy.init()
-    node = Node('python_node1')
-    node.get_logger().info('你好python 节点！')
-    rclpy.spin(node)
-    rclpy.shutdown()
