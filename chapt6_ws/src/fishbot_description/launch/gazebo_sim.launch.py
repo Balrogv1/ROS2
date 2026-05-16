db99aa67f60ab86b0fb77c7a08d10df361ea19bb
Balrogv1 <2424610757@qq.com>
2026-05-16 13:32:59 +0800

    Update fishbot LAUNCH description

---
+++ chapt6_ws/src/fishbot_description/launch/gazebo_sim.launch.py
@@ -0,0 +1,44 @@
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    # 获取功能包share路径
+    urdf_package_path = get_package_share_directory('fishbot_description')
+    default_xacro_path = os.path.join(urdf_package_path, 'urdf', 'fishbot/fishbot.urdf.xacro')
+    #defailt_rviz_config_path = os.path.join(urdf_package_path, 'config', 'display_roboy_model.rviz')
+    defailt_gazebo_world_path = os.path.join(urdf_package_path, 'world', 'custom_room.world')
+
+    # 声明参数
+    action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
+        name='model',default_value=str(default_xacro_path),description='加载的模型文件路径'
+    )
+    # 通过文件路径，获取内容, 并转化为参数、
+    Substitutions_command_result = launch.substitutions.Command(['xacro ',launch.substitutions.LaunchConfiguration('model')])
+    robot_description_value = launch_ros.parameter_descriptions.ParameterValue(Substitutions_command_result,value_type=str)
+
+    action_robot_state_publisher = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description_value}] 
+    )
+    
+    action_launch_gazebo = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            [get_package_share_directory('gazebo_ros'),'/launch','/gazebo.launch.py']
+        ),
+        launch_arguments=[('world',defailt_gazebo_world_path),('verbose','true')]
+    )
+    # action_rviz_node = launch_ros.actions.Node(
+    #     package='rviz2',
+    #     executable='rviz2',
+    #     arguments=['-d',defailt_rviz_config_path],
+    # )
+
+    return launch.LaunchDescription([
+        action_declare_arg_mode_path, 
+        action_robot_state_publisher,
+        action_launch_gazebo,
+        # action_rviz_node,
+    ])
